93e51d090e28484bad6240ec8d068dce5c81ad93
merdigon <szymon.nowak3848@gmail.com>
2016-03-10 09:22:15 +0100

    Poprawa struktury projektu Space i dodanie atrybutów dla DTO

--- Lab3/Space/Space/DataTransferObjects/Person.cs
+++ Lab3/Space/Space/DataTransferObjects/Person.cs
@@ -1,13 +1,18 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.Serialization;
 using System.Text;
 
 namespace Model_Struct.Space
 {
+    [DataContract]
     public class Person
     {
+        [DataMember]
         public string Name { get; set; }
+
+        [DataMember]
         public int Age { get; set; }
     }
 }
--- Lab3/Space/Space/DataTransferObjects/Starship.cs
+++ Lab3/Space/Space/DataTransferObjects/Starship.cs
@@ -1,14 +1,21 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.Serialization;
 using System.Text;
 
 namespace Model_Struct.Space
 {
+    [DataContract]
     public class Starship
     {
+        [DataMember]
         public string Name { get; set; }
+
+        [DataMember]
         public Person Captain { get; set; }
+
+        [DataMember]
         public List<Person> Crew { get; set; }
     }
 }
