7e9932b6120f264c43e3a5d76627ce42a19e130c
merdigon <szymon.nowak3848@gmail.com>
2016-03-14 15:57:54 +0100

    Dodane zadania z Lab1(opcjonalne)

---
+++ Lab1 (opcjonalny)/Lab1/Zadanie2/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zadanie2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = Int32.Parse(args[0]);
+            int b = Int32.Parse(args[1]);
+
+            int temp;
+            int ab = a * b;
+            while(b != 0)
+            {
+                temp = b;
+                b = a % b;
+                a = temp;
+            }
+
+            Console.WriteLine(ab / a);
+        }
+    }
+}
