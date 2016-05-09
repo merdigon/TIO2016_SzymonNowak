18ee69c78fbd1cfd786dfa5d42fe648687db2de1
merdigon <szymon.nowak3848@gmail.com>
2016-05-09 13:09:33 +0200

    Finalna wersja Lab8

---
+++ Lab8/RestService/Loggers/Logger.cs
@@ -0,0 +1,21 @@
+﻿using log4net;
+using log4net.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace RestService.Loggers
+{
+    public class Logger
+    {
+        private static readonly ILog log =
+           log4net.LogManager.GetLogger(typeof(Logger));
+
+        public void LogInfo(string message)
+        {
+            if (log != null)
+                log.Info(message);
+        }
+    }
+}
